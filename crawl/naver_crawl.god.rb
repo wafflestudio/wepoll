33c7432cadc8dd6ae17e31f49adaeb335e253c09
Jineok Kim <jineok12@Jineok-ui-MacBook-Air.local>
2012-02-04 18:20:26 +0900

    naver crawling

---
+++ crawl/naver_crawl.god.rb
@@ -0,0 +1,8 @@
+ruby_path = "/home/jineok12/.rvm/rubies/ruby-1.9.2-p290/bin/ruby"
+God.watch do |w|
+  w.dir = Dir.pwd
+  w.name = "naver_crawler"
+  w.start = "#{ruby_path} naver.rb"
+  w.log = "naver_crawl.log"
+  w.keepalive
+end
