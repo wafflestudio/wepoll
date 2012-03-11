147b8e02314446932dfd0021aac815f6f4ba0ffc
Jaehwa Han <drh@wafflestudio.com>
2012-03-11 22:45:20 +0900

    various bug fix

--- rails_app/app/assets/javascripts/login_info.js.coffee
+++ rails_app/app/assets/javascripts/login_info.js.coffee
@@ -1,10 +1,8 @@
 show_join_form = () ->
   $.colorbox {href:"<%= new_user_registration_path%>"}
 
-
 $ () ->
   $(".popup-auth").click () ->
     $this = $ this
     href = $this.attr "href"
-    window.open href, "authWindow", "width=600,height=400,menubar=0,location=0,toolbar=0,modal=yes,alwaysRaised=yes"
-    return false
+    return show_auth_dialog(href)
--- rails_app/app/controllers/users/sessions_controller.rb
+++ rails_app/app/controllers/users/sessions_controller.rb
@@ -1,16 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-    resource = build_resource
-    clean_up_passwords(resource)
-    respond_to do |format|
-      format.js {render :js => '$.colorbox({href:"/users/sign_in", width: 520, height: 250});'}
-      format.html {
-        session[:need_login] = true
-        render '/devise/sessions/new2.html.erb'
-      }
-    end
-#    respond_with(resource, serialize_options(resource))
-#    render :js => '$.colorbox({href:"/users/sign_in", width: 520, height: 250});'
+    render :layout => false
   end
 
   def after_sign_in_path_for(resource)
