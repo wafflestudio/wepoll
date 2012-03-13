6cd80378f281477e7993290f9caa1f03bf7cbce4
Taekmin Kim <tantara.tm@gmail.com>
2012-03-14 05:07:35 +0900

    popular link -> looks like facebook & added infinitescroll

--- rails_app/script/get_18th_politician_from_19th.rb
+++ rails_app/script/get_18th_politician_from_19th.rb
@@ -1,10 +1,15 @@
 #encoding: utf-8
 require 'csv'
 
-CSV.foreach(Rails.root+"init_data/center.csv") do |line|
+CSV.foreach(Rails.root+"init_data/center.csv", :encoding => "UTF-8") do |line|
   party = line[1]
   name = line[3]
-
+  district = line[0]
+  
+  if party != "무소속" && party != "새누리당" && party != "민주통합당" && party != "통합진보당" && party != "자유선진" && party != "국민생각당"
+    printf name + "(" + party + ", " + district + ") "
+  end
+=begin
   p = Politician.where(name: name, party: party).first 
 
   if !p.nil?
@@ -14,4 +19,26 @@ CSV.foreach(Rails.root+"init_data/center.csv") do |line|
   else
     #printf "#{name}(#{party}) "
   end
+=end
+end
+
+CSV.foreach(Rails.root+"init_data/center_busan.csv", :encoding => "UTF-8") do |line|
+  party = line[1]
+  name = line[3]
+  district = line[0]
+  
+  if party != "무소속" && party != "새누리당" && party != "민주통합당" && party != "통합진보당" && party != "자유선진" && party != "국민생각당"
+    printf name + "(" + party + ", " + district + ") "
+  end
+=begin
+  p = Politician.where(name: name, party: party).first 
+
+  if !p.nil?
+    #printf "#{name}(#{party}) "
+    p.candidate = true
+    p.save
+  else
+    #printf "#{name}(#{party}) "
+  end
+=end
 end
