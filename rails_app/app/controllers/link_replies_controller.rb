bd3b4954eb4ac1aedb1658a66947fc31a4bbefce
Jaehwa Han <drh@wafflestudio.com>
2012-03-15 22:36:44 +0900

    댓글오류수정

--- rails_app/app/controllers/link_replies_controller.rb
+++ rails_app/app/controllers/link_replies_controller.rb
@@ -4,15 +4,13 @@ class LinkRepliesController < ApplicationController
   layout false
   def list
     @entry = TimelineEntry.find(params[:link_reply_id])
-    @replies = @entry.link_replies[0..@entry.link_replies.count - 3]
+    @replies = @entry.link_replies[0...@entry.link_replies.count - 3]
   end
   def show
-    Rails.logger.info "=================hello"
     @link_reply = LinkReply.find(params[:id])
   end
 
   def create
-    Rails.logger.info "=================hi"
     @reply = LinkReply.new(params[:link_reply])
     @reply.user = current_user
     @reply.save
